115de7c086d4f96ff72219e7ed284f345fac26e6
Mariusz Marzec <mariusz.marzec00@gmail.com>
2021-12-22 16:08:24 +0100

    Added view for showing progress

--- common/src/commonMain/kotlin/com/marzec/common/App.kt
+++ common/src/commonMain/kotlin/com/marzec/common/App.kt
@@ -1,20 +1,44 @@
 package com.marzec.common
 
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
+import androidx.compose.material.LinearProgressIndicator
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
 
-    Button(onClick = {
-        text = "Hello"
-    }) {
-        Text(text)
+    ProgressRun(
+        progress = 0.6f,
+        onStartButtonClick = { }
+    )
+}
+
+@Composable
+fun ProgressRun(
+    progress: Float,
+    onStartButtonClick: () -> Unit
+) {
+    Column {
+        Row(modifier = Modifier.padding(16.dp), verticalAlignment = Alignment.CenterVertically) {
+            Button(onClick = {
+                onStartButtonClick.invoke()
+            }) {
+                Text("Start")
+            }
+            Spacer(Modifier.width(16.dp))
+
+            LinearProgressIndicator(
+                progress = progress,
+                color = Color.LightGray,
+                backgroundColor = Color.DarkGray,
+                modifier = Modifier.width(500.dp).height(50.dp)
+            )
+        }
     }
 }
