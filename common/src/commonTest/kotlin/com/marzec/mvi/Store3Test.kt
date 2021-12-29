9e8f957c2958f16527cd779aa54b67e5a4119be3
Mariusz Marzec <mariusz.marzec00@gmail.com>
2021-12-29 22:00:41 +0100

    Added tests

--- common/src/commonTest/kotlin/com/marzec/mvi/Store3Test.kt
+++ common/src/commonTest/kotlin/com/marzec/mvi/Store3Test.kt
@@ -1,16 +1,17 @@
 package com.marzec.mvi
 
-import com.marzec.core.TestCollector
 import com.marzec.core.runStoreTest
-import com.marzec.core.test
 import io.mockk.coEvery
 import io.mockk.mockk
-import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.*
-import kotlin.test.Test
-import kotlin.test.BeforeTest
 import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
 
 
 @OptIn(ExperimentalCoroutinesApi::class)
@@ -52,5 +53,63 @@ class Store3Test {
 
         values.isEqualTo(0, 1, 2)
         coEvery { func.invoke(any()) }
+    }
+
+    @Test
+    fun `checks calling second time intent with id, kills previous one`() = runStoreTest(dispatcher, 0) {
+        store.intent<Int>("id") {
+            onTrigger {
+                flow {
+                    delay(1000)
+                    emit(1)
+                    delay(1000)
+                    emit(-1)
+                }
+            }
+
+            reducer { resultNonNull() }
+        }
+
+        testScope.advanceTimeBy(1100)
+
+        store.intent<Int>("id") {
+            onTrigger { flowOf(2) }
+
+            reducer { resultNonNull() }
+        }
+        testScope.advanceTimeBy(1000)
+
+        values.isEqualTo(0, 1, 2)
+    }
+
+    @Test
+    fun `checks if running few intents in side effect works`() = runStoreTest(dispatcher, 0) {
+
+        store.sideEffectIntent {
+
+            store.intent<Unit> {
+                reducer { 1 }
+            }
+
+            testScope.advanceUntilIdle()
+
+            store.intent<Unit> {
+                reducer { 2 }
+            }
+        }
+
+        values.isEqualTo(0, 1, 2)
+    }
+
+    @Test
+    fun `checks if initial action is called`() = runStoreTest(dispatcher, 0) {
+        store.init {
+            store.intent<Unit> {
+                reducer { 1 }
+            }
+            testScope.advanceUntilIdle()
+        }
+
+        values.isEqualTo(0, 1)
     }
 }
