93db11b52eca894a8ac5ce7cc215c2f87c102e62
Mariusz Marzec <mariusz.marzec00@gmail.com>
2022-01-11 10:18:14 +0100

    Removed logging

--- common/src/commonMain/kotlin/com.marzec.mvi/mvi.kt
+++ common/src/commonMain/kotlin/com.marzec.mvi/mvi.kt
@@ -52,7 +52,6 @@ open class Store3<State : Any>(
                     onNewState(it.state!!)
                     it.sideEffect?.invoke(it.result, it.state)
                 }.collect {
-                    println("STATE: $it")
                     _state.emit(it.state!!)
                 }
         }
